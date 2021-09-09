7677318f04f9c27a317c5b7f9cda4e149ef52978
Patainea Robert <patainearobert@yahoo.com>
2021-09-09 15:44:22 +0300

    Created cell and board

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Cell from './Cell';
 
 function App() {
+
+
+  const renderRow = (columns: number) => {
+    return new Array(columns).fill(0).map(() => {
+      return <Cell />
+    })
+  }
+
+  const renderBoard = (rows: number, columns: number) => {
+    return new Array(rows).fill(0).map(() => {
+      return <div className="row">
+        {renderRow(columns)}
+      </div>
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {renderBoard(9, 9)}
+       </div>
     </div>
   );
 }
